1f4425ab440de2891f01f4687a56ad111cc3eda0
mariocstweb <mariocstmra@gmail.com>
2024-01-22 19:55:24 +0100

    added methods to button

--- js/script.js
+++ js/script.js
@@ -33,6 +33,27 @@ const app = createApp({
     ],
     currentIndex: 0,
   }),
+  computed: {
+    lastElementIndex() {
+      return this.pictures.length - 1;
+    },
+    isFirstIndex() {
+      return this.currentIndex === 0;
+    },
+    isLastIIndex() {
+      return this.currentIndex === this.pictures.length - 1;
+    },
+  },
+  methods: {
+    goToNext() {
+      if (this.isLastIndex) this.currentIndex = 0;
+      else this.currentIndex++;
+    },
+    goToPrev() {
+      if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
+      else this.currentIndex--;
+    },
+  },
 });
 
 app.mount("#root");
